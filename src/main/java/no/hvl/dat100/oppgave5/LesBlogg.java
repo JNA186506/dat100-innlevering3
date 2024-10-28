4dfd3a68124bfc35ab6d2b8a368ffa0446718766
JNA186506 <186506@stud.hvl.no>
2024-10-28 23:00:56 +0100

    Oppgave 5 start...

--- src/main/java/no/hvl/dat100/oppgave5/LesBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave5/LesBlogg.java
@@ -19,9 +19,36 @@ public class LesBlogg {
 
 	private static String TEKST = "TEKST";
 	private static String BILDE = "BILDE";
+	private static int id;
+	private static String bruker;
+	private static String dato;
+	private static String tekst;
+	private static String url;
 
 	public static Blogg les(String mappe, String filnavn) {
 
+		File fil = new File(mappe, filnavn); //Scuffa drit...
+		Blogg b = new Blogg();
 
-	}
+		try (Scanner scanner = new Scanner(fil)) {
+
+			while (scanner.hasNextLine()) {
+
+				if (scanner.nextInt() == b.getAntall()) {
+					int antall = scanner.nextInt();
+					b = new Blogg(antall);
+				}
+				if (scanner.nextLine().equals(TEKST)) {
+					Tekst tekstInnlegg = new Tekst(id,bruker,dato,tekst); //Er dette en akseptabel fremgangsmåte?
+				} else if (scanner.nextLine().equals(BILDE)) {
+					Bilde bildeInnlegg = new Bilde(id,bruker,dato,tekst,url);
+
+				}
+			}
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return b;
+    }
 }
