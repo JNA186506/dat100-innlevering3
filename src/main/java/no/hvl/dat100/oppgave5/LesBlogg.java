34e10ad2f4757611dd88d9546bb10a4ca46a185d
JNA186506 <186506@stud.hvl.no>
2024-10-29 11:20:45 +0100

    Oppgave 5 suksess

--- src/main/java/no/hvl/dat100/oppgave5/LesBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave5/LesBlogg.java
@@ -19,36 +19,48 @@ public class LesBlogg {
 
 	private static String TEKST = "TEKST";
 	private static String BILDE = "BILDE";
-	private static int id;
-	private static String bruker;
-	private static String dato;
-	private static String tekst;
-	private static String url;
 
 	public static Blogg les(String mappe, String filnavn) {
 
-		File fil = new File(mappe, filnavn); //Scuffa drit...
+		File file = new File(mappe, filnavn);
 		Blogg b = new Blogg();
 
-		try (Scanner scanner = new Scanner(fil)) {
+		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+			String line = br.readLine();
+			int antall = Integer.parseInt(line);
 
-			while (scanner.hasNextLine()) {
+			for (int i = 0; i < antall; i++) {
+				line = br.readLine();
+				if (line.equals(TEKST)) {
+					int id = Integer.parseInt(br.readLine());
+					String bruker = br.readLine();
+					String dato = br.readLine();
+					int likes = Integer.parseInt(br.readLine());
+					String tekst = br.readLine();
 
-				if (scanner.nextInt() == b.getAntall()) {
-					int antall = scanner.nextInt();
-					b = new Blogg(antall);
+					Tekst tekstInnlegg = new Tekst(id,bruker,dato,likes,tekst);
+					b.leggTil(tekstInnlegg);
+				} else if (line.equals(BILDE)) {
+					int id = Integer.parseInt(br.readLine());
+					String bruker = br.readLine();
+					String dato = br.readLine();
+					int likes = Integer.parseInt(br.readLine());
+					String tekst = br.readLine();
+					String url = br.readLine();
+
+					Bilde bildeInnlegg = new Bilde(id,bruker,dato,likes,tekst,url);
+					b.leggTil(bildeInnlegg);
 				}
-				if (scanner.nextLine().equals(TEKST)) {
-					Tekst tekstInnlegg = new Tekst(id,bruker,dato,tekst); //Er dette en akseptabel fremgangsmåte?
-				} else if (scanner.nextLine().equals(BILDE)) {
-					Bilde bildeInnlegg = new Bilde(id,bruker,dato,tekst,url);
 
-				}
 			}
-			
+
 		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+			throw new RuntimeException("File not found:" + file, e);
+		} catch (IOException e) {
+			throw new RuntimeException("Error reading file:" + file, e);
 		}
+
+
 		return b;
     }
 }
